e8755a3c69f1e17baf409a46ba98201a93162e61
cgalindo <cgalindo@indeed.com>
2020-04-13 21:42:46 -0500

    Add theme

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { ThemeProvider } from 'emotion-theming';
 
 import App from './components/App/App';
 import * as serviceWorker from './utils/serviceWorker';
+import theme from './utils/theme';
 
 ReactDOM.render(
     <React.StrictMode>
-        <App />
+        <ThemeProvider theme={theme}>
+            <App />
+        </ThemeProvider>
     </React.StrictMode>,
     document.getElementById('root'),
 );
