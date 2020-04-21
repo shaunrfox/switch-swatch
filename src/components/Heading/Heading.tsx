256e7c1341f818d63ab53958ef0b6256fb473382
cgalindo <cgalindo@indeed.com>
2020-04-20 19:10:42 -0500

    Rework emotion and the theme

--- src/components/Heading/Heading.tsx
+++ src/components/Heading/Heading.tsx
@@ -1,20 +1,23 @@
 import React from 'react';
 import styled from '@emotion/styled';
 
-interface HeadingProps {
+import { themeHelper, sxPropHelper, StyleProps } from '../../utils/styled';
+
+interface HeadingProps extends StyleProps {
     children: React.ReactNode;
     level: 1 | 2 | 3 | 4 | 5;
-    as?: React.ElementType;
 }
 
-const Heading = styled.h2<HeadingProps>((props) => ({
-    fontSize: (props.theme as any).fontSizes[props.level],
-    lineHeight: '125%',
-    fontFamily: 'Source Sans Pro, sans-serif',
-    fontWeight: 'bold',
-    color: (props.theme as any).colors.neutral[9],
-    '-webkit-font-smoothing': 'antialiased',
-    '-moz-osx-font-smoothing': 'grayscale',
-}));
+const Heading = styled.h2<HeadingProps>(
+    ({ level, ...props }) =>
+        themeHelper({
+            fontSize: level,
+            lineHeight: '125%',
+            fontFamily: 'Source Sans Pro, sans-serif',
+            fontWeight: 'bold',
+            color: 'neutral.9',
+        })(props),
+    sxPropHelper,
+);
 
 export default Heading;
